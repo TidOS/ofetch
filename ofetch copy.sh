531f27873e781d8e72082995899854f74bf0a5b8
Jordan Thomas <jordan@jmthomas.tech>
2020-06-17 01:25:55 -0500

    added some basic stats, script for handling bash commands.  dealing with pipes in python was too annoying

--- ofetch copy.sh
+++ ofetch copy.sh
@@ -5,7 +5,6 @@ import blessings
 from blessings import Terminal
 import tempfile
 
-info_path="./ofetch-get-info.sh"
 system('reset')
 
 #rainbow style
@@ -13,6 +12,7 @@ logo_colors=[10,10,10,11,11,3,3,9,9,8,8,12,12,12,5]
 
 #gray
 #logo_colors=[7,7,7,7,7,7,7,7,7,7,7,7,7,7,7]
+
 
 logo_lines=["                :++++.         ",
 "               /+++/.          ",
@@ -31,10 +31,19 @@ logo_lines=["                :++++.         ",
 "      \`:+oo+/:-..-:/+o+/-   "]
 term = Terminal()
 
-infos = ["MACOS", "MODEL", "CPU", "GPU", "THERM", "DISK", "SHELL", "TERM"]
+infos = ["SYSTEM", "CPU"]
 
 tempfile = tempfile.NamedTemporaryFile(mode = 'w')
 readfile = open(tempfile.name)
+# system
+system("uname -rms > " + tempfile.name)
+SYSTEM=term.bold("SYSTEM: " + term.green + readfile.readline() + term.normal)
+#cpu
+system("sysctl -n machdep.cpu.brand_string > " + tempfile.name)
+CPU=term.bold("CPU:    " + term.green + readfile.readline() + term.normal)
+#ram
+system("sysctl -n hw.memsize | awk \'{printf(\"%d\", $1/1e+06)}\' > " + tempfile.name)
+RAM=term.bold("RAM:   " + term.green + readfile.readline() + " " + term.green + "MiB" + term.normal)
 
 logo_width=0
 line=0
@@ -46,16 +55,13 @@ for i in logo_lines:
         logo_width=len(i)
 XText = logo_width + 3
 
+with term.location(XText,y=0):
+    print(SYSTEM)
+with term.location(XText,y=1):
+    print(CPU)
+with term.location(XText, y=2):
+    print(RAM)
 
-row = 0;
-for i in infos:
-    #print("about to do " + i + "on row " + str(row))
-    system(info_path + " " + i + " >> " + tempfile.name)
-    with term.location(XText,y=row):
-        print(term.bold_underline(i.rjust(5)) + ": " + term.green + readfile.readline().ljust(20) + term.normal)
-    row = row + 1
-#pint(tempfile.name)
-#system("cat " + tempfile.name)
 tempfile.close()
 readfile.close()
 
---
+++ ofetch-get-info.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+
+case $1 in
+MACOS)
+    # MacOS version
+    sw_vers | awk -F "\:\t" '{printf "%s",$2}'
+    echo
+    ;;
+CPU)
+    # CPU
+    sysctl -n machdep.cpu.brand_string
+    ;;
+RAM)
+    # RAM in MB
+    sysctl -n hw.memsize|awk '{printf("%d", $1/1e+06)}'
+    ;;
+MODEL)
+    # Mac model
+    sysctl -n hw.model
+    ;;
+GPU)
+    # Video card
+    system_profiler SPDisplaysDataType|awk -F"\:\ " '/Chipset|VRAM/{printf "%s ",$2}'
+    echo
+    ;;
+
+THERM)
+    # temperature in C - fails on hackintosh
+    sudo powermetrics --samplers smc -i1 -n1 | grep -i "CPU die" | awk '{printf("%s °C",$4)}'
+    echo
+    ;;
+
+DISK)
+    # rootfs disk usage
+    df -h / | tail -n 1 | sed 's/Gi//g' | awk '{printf("%s/%s GiB free\n",$4,$2)}'
+    ;;
+    
+KERNEL)
+    #kernel
+    uname -rms
+    ;;
+    
+SHELL)
+    #shell
+    echo "zsh"
+    ;;
+    
+TERM)
+    #terminal
+    echo $TERM
+    ;;
+    
+*)
+    echo DUMMY
+    ;;
+esac
